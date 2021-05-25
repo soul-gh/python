f81a3ced43260622818b0e1a2a4d60f02d5e9888
soul <g373733920@gmail.com>
2021-05-25 10:28:45 +0900

    5

--- 4/hello.py
+++ 4/hello.py
@@ -44,3 +44,49 @@ print(dimensions[1])
 print(dimensions)
 dimensions=(100,50)
 print(dimensions)
+print("\n")
+
+cars=['audi','bmw','subaru','toyota']
+for car in cars:
+    if car =='bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+car='Audi'
+if car=='audi':
+    print("true")
+if car.lower() == 'audi':
+    print("true")
+age_0=22
+age_1=18
+if age_0>=21 and age_1>=17:
+    print("true")
+if age_0>22 or age_1>19:
+    print("true")
+else:
+    print("false")
+requested_toppings=['mushroom','onions','pineapple']
+if 'mushroom' in requested_toppings:
+    print("T")
+if 'pepperoni' not in requested_toppings:
+    print("F")
+age= 17
+if age >=18:
+    print("You are old enough to vote.")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
+if age<4:
+    print("You admission cost is $0.")
+elif age<18:
+    print("You admission cost is $5.")
+else:
+    print("You admission cost is $10.")
+if age<4:
+    price=0
+elif age<18:
+    price=5
+else:
+    price=10
+print("You admission cost is $"+str(price)+".")
--- 4/practice.py
+++ 4/practice.py
@@ -56,4 +56,64 @@ print("\n")
 supermarket=('egg','milk','potatochips','panapple','banana')
 for food1 in supermarket:
     print(food1)
+print('\n')
 
+foods=['egg','apple','banana','bread']
+food=foods[0]
+print("Is food=='egg'?I predict True.")
+print(food=='egg')
+print("Is food=='apple'?I predict False.")
+print(food=='apple')
+print('he'!='she')
+print(food.title()=='egg')
+print('\n')
+answer=10
+print(answer==10)
+print(answer!=10)
+print(answer<11)
+print(answer>9)
+print(answer<=9)
+print(answer>=9)
+print(answer>=10 and answer<=10)
+print(answer>20 or answer<15)
+print('\n')
+print('egg' in foods)
+print('apple'not in foods)
+alien_color='yellow'
+if alien_color=='yellow':
+    print("You got 5 point.")
+if alien_color=='green':
+    print("You got 5 point.")
+alien_color='red'
+if alien_color=='green':
+    print("You got 5 point.")
+elif alien_color=='yellow':
+    print("You got 10 point.")
+elif alien_color=='red':
+    print("You got 15 point.")
+
+age=13
+if age<2:
+    print("You are baby.")
+elif age>=2 and age<4:
+    print("You are learning how to stand.")
+elif age>=4 and age<13:
+    print("You are child.")
+elif age>=13 and age<20:
+    print("You are teenager.")
+elif age>=20 and age<65:
+    print("You are adult.")
+else:
+    print("You are old people.")
+
+favorate_fruits=['apple','banana','kiwi']
+if 'apple' in favorate_fruits:
+    print("You really like apple.")
+if 'banana' in favorate_fruits:
+    print("You really like banana.")
+if 'kiwi' in favorate_fruits:
+    print("You really like kiwi.")
+if 'pear' not in favorate_fruits:
+    print("You don't like pear.")
+if 'paneapple' in favorate_fruits:
+    print("You really like paneapple.")
