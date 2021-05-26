d837ac7a24076495718b7912274be74778324cd2
soul <g373733920@gmail.com>
2021-05-26 10:58:24 +0900

    5.4

--- 5/hello.py
+++ 5/hello.py
@@ -41,4 +41,23 @@ elif age<18:
     price=5
 else:
     price=10
-print("You admission cost is $"+str(price)+".")
+print("You admission cost is $"+str(price)+".\n")
+
+available_toppings=['mushrooms','green peppers','extra cheese','olives','pepperoni','pineapple']
+requested_toppings=['mushrooms','green peppers','extra cheese']
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping=='green peppers':
+                print("Sorry,we are out of green peppers right now.")
+        else:
+                print("Adding "+requested_topping+".")
+    print("Finished making your pizza!")
+else:
+    print("Are you sure you want a pizza?")
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print("Adding "+requested_topping+".")
+    else:
+        print("Sorry, we don't have "+requested_topping+'.')
+print("Finished making your pizza!")
--- 5/practice.py
+++ 5/practice.py
@@ -56,4 +56,35 @@ if 'kiwi' in favorate_fruits:
 if 'pear' not in favorate_fruits:
     print("You don't like pear.")
 if 'paneapple' in favorate_fruits:
-    print("You really like paneapple.")
+    print("You really like paneapple.\n")
+
+#users=['admin','tom','jerry','dingding','lala']
+users=[]
+if users:
+    for user in users:   
+        if user == 'admin':
+            print("Hello Admin, would you like to see a status report?")
+        else :
+            print("Hello "+user.title()+", thank you for logging in again.")
+else :
+    print("We need to find some users.")
+
+current_users=['admin','tom','jerry','dingding','lala']
+new_users=['admin','Tom','dixi','po','makabaka']
+for new_user in new_users:
+    if new_user in current_users:
+        print("Please enter another username.")
+    elif new_user.lower() in current_users:
+        print("Please enter another username.")
+    else :
+        print(new_user+" haven't been used.")
+nums=[1,2,3,4,5,6,7,8,9]
+for num in nums:
+    if num == 1:
+        print(str(num)+"st")
+    elif num == 2:
+        print(str(num)+"nd")
+    elif num == 3:
+        print(str(num)+"rd")
+    else:
+        print(str(num)+"th")
